c65b5b056e03e0e54ce96f1af21b2623eb38261a
MonikaFu <monika.furdyna@gmail.com>
2026-04-09 19:55:20 +0200

    Add project structure

---
+++ src/data_cleaning.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+gdp = pd.read_csv('data/raw/gdp-per-capita-maddison-project-database/gdp-per-capita-maddison-project-database.csv')
+#life_expentancy
+#calories
+#vegetables
+#income_groups
